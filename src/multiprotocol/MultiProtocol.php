c49a360816e8a231cf730a9e0a18d47c63f77153
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-04-21 14:57:25 -0700

    support current protocol and above

--- src/multiprotocol/MultiProtocol.php
+++ src/multiprotocol/MultiProtocol.php
@@ -9,6 +9,7 @@ use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
 /**
  * Class MultiProtocol
@@ -25,17 +26,15 @@ class MultiProtocol extends PluginBase implements Listener {
      * @param DataPacketReceiveEvent $event
      */
     public function onLogin(DataPacketReceiveEvent $event) {
-
         $pk = $event->getPacket();
-
-        if(!$pk instanceof LoginPacket) {
+        if (!$pk instanceof LoginPacket) {
             return;
         }
-
         $player = $event->getPlayer();
         $currentProtocol = ProtocolInfo::CURRENT_PROTOCOL;
-
-        if($pk->protocol !== $currentProtocol) {
+        if ($pk->protocol < $currentProtocol) {
+            $player->kick(TextFormat::RED . "Your protocol version needs to be same or above " . $currentProtocol);
+        } elseif ($pk->protocol > $currentProtocol) {
             $pk->protocol = $currentProtocol;
             $this->getLogger()->alert("§6{$player->getName()}'s protocol changed to {$currentProtocol}!");
         }
