139ff99488f17e65d9667b338511dc5279274427
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-06-19 12:06:23 -0700

    Add protocol versions

--- src/MultiProtocol/protocol/Info.php
+++ src/MultiProtocol/protocol/Info.php
@@ -6,4 +6,61 @@ use pocketmine\network\mcpe\protocol\ProtocolInfo;
 
 class Info extends ProtocolInfo {
 
+    public const CURRENT_PROTOCOL = 120;
+    public const ACCEPTED_PROTOCOLS = [];
+
+    public const PROTOCOL_134 = 134; // 1.2.0.20, 1.2.0.22
+    public const PROTOCOL_135 = 135; // 1.2.0.24, 1.2.0.25
+    public const PROTOCOL_136 = 136; // 1.2.0.31
+    public const PROTOCOL_137 = 137; // 1.2.0
+    public const PROTOCOL_140 = 140; // 1.2.5.11
+    public const PROTOCOL_141 = 141; // 1.2.5.x
+    public const PROTOCOL_150 = 150; // 1.2.6
+    public const PROTOCOL_160 = 160; // 1.2.7
+    public const PROTOCOL_200 = 200; // 1.2.10
+    public const PROTOCOL_201 = 201; // 1.2.10.x
+    public const PROTOCOL_220 = 220; // 1.2.13.5
+    public const PROTOCOL_221 = 221; // 1.2.13.8
+    public const PROTOCOL_222 = 222; // 1.2.13.10
+    public const PROTOCOL_223 = 223; // 1.2.13.54
+    public const PROTOCOL_224 = 224; // 1.2.13.11
+    public const PROTOCOL_240 = 240; // 1.2.14.2
+    public const PROTOCOL_250 = 250; // 1.2.15.1
+    public const PROTOCOL_260 = 260; // 1.2.20.1, 1.2.20.2
+    public const PROTOCOL_261 = 261; // 1.4.0
+    public const PROTOCOL_270 = 270; // 1.5.0.0
+    public const PROTOCOL_271 = 271; // 1.5.0.0, 1.5.0.4
+    public const PROTOCOL_273 = 273; // 1.5.0.7
+    public const PROTOCOL_274 = 274; // 1.5.0.10
+    public const PROTOCOL_280 = 280; // 1.6.0.1
+    public const PROTOCOL_281 = 281; // 1.6.0.5
+    public const PROTOCOL_282 = 282; // 1.6.0.8
+    public const PROTOCOL_290 = 290; // 1.7.0.2
+    public const PROTOCOL_291 = 291; // 1.7.0.5
+    public const PROTOCOL_310 = 310; // 1.8.0.4, 1.8.0.8
+    public const PROTOCOL_311 = 311; // 1.8.0.9, 1.8.0.10
+    public const PROTOCOL_312 = 312; // 1.8.0.11
+    public const PROTOCOL_313 = 313; // 1.8.0 rc1
+    public const PROTOCOL_330 = 330; // 1.9.0.0
+    public const PROTOCOL_331 = 331; // 1.9.0.2
+    public const PROTOCOL_332 = 332; // 1.9.0.3
+    public const PROTOCOL_340 = 340; // 1.10.0.3
+    public const PROTOCOL_342 = 342; // 1.10.0
+    public const PROTOCOL_350 = 350; // 1.11.0.1
+    public const PROTOCOL_351 = 351; // 1.11.0.3
+    public const PROTOCOL_352 = 352; // 1.11.0.4
+    public const PROTOCOL_353 = 353; // 1.11.0.5
+    public const PROTOCOL_354 = 354; // 1.11.0.7, 1.11.1, 1.11.2, 1.11.3
+    public const PROTOCOL_360 = 360; // 1.12.0.2
+    public const PROTOCOL_361 = 361; // 1.12.0.3, 1.12.0.4, 1.12.0.5, 1.12.0.6
+    public const PROTOCOL_370 = 370; // 1.13.0.1
+    public const PROTOCOL_371 = 371; // 1.13.0.4, 1.13.0.5, 1.13.0.6
+    public const PROTOCOL_385 = 385; // 1.13.0.7, 1.13.0.9, 1.13.0.10
+    public const PROTOCOL_386 = 386; // 1.13.0.12
+    public const PROTOCOL_387 = 387; // 1.13.0.15
+    public const PROTOCOL_388 = 388; // 1.13.0.25
+    public const PROTOCOL_389 = 389; // 1.14.x.x
+    public const PROTOCOL_390 = 390; // 1.14.60
+    public const PROTOCOL_392 = 392; // 1.15.0.51
+
 }
