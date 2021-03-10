cdf2a669c8fccafad51a5509e56ade5a5b566297
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-03-09 23:56:40 -0800

    Create Packet.php

---
+++ src/MultiProtocol/Network/Protocol/Packet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MultiProtocol\Network\Protocol;
+
+use pocketmine\network\protocol\DataPacket;
+use MultiProtocol\Network\Protocol\Info;
+/*
+abstract class Packet extends DataPacket {
+
+    public final static function convertProtocol($protocol) {
+        switch ($protocol) {
+            
+            default:
+                return Info::PROTOCOL_120;
+        }
+    }
+
+}*/
