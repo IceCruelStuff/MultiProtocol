4133011e67bbc06b1bb077be38f066a3c5c77afd
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-09-29 23:11:04 -0700

    Update NetworkSessionAdapter.php

--- src/MultiProtocol/Network/NetworkSessionAdapter.php
+++ src/MultiProtocol/Network/NetworkSessionAdapter.php
@@ -8,11 +8,11 @@ use pocketmine\network\mcpe\PlayerNetworkSessionAdapter;
 use MultiProtocol\Network\Protocol\Info;
 
 class NetworkSessionAdapter extends PlayerNetworkSessionAdapter {
-/*
+
     public function handleLogin(LoginPacket $packet) : bool {
         if (!in_array($packet->protocol, Info::ACCEPTED_PROTOCOLS)) {
             return;
         }
     }
-*/
+
 }
